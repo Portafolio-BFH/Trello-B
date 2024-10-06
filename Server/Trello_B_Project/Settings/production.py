6b2b86f3ea991491fed2cf2b51dcdd9eed11a045
fbfranco <fbfranco29@gmail.com>
2024-10-06 01:09:34 -0400

    Settings: Generals Trello_B_Project

---
+++ Server/Trello_B_Project/Settings/production.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'mssql',
+        'NAME': 'Trello_B',
+        'HOST': 'localhost',
+        'PORT': 1433,
+        'USER': 'sa',
+        'PASSWORD': '72130870Aa',
+        'TRUSTED_CONNECTION': 'no'
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
--- Server/Trello_B_Project/asgi.py
+++ Server/Trello_B_Project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trello_B_Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trello_B_Project.Settings.local')
 
 application = get_asgi_application()
